1f33f64ae65c83a9e8aa6ee83ad883b36cc3661b
Daniel <daniel.silvamiranda02@gmail.com>
2022-01-27 12:37:44 -0300

    test: add test SignupController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,7 +4,6 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      // name: 'any_name',
       email: 'any_email!',
       password: 'any_password!',
       passwordConfirmation: 'any_password_confirmation!'
