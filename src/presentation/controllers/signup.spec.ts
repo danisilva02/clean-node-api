27cee216957c09236267eef0fafd05dfa97a5ea0
Daniel <daniel.silvamiranda02@gmail.com>
2022-01-27 23:39:16 -0300

    refactor: move all protocol to a simple file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 import { MissingParamName, ServerError } from '../erros'
-import { EmailValidator } from '../protocols/email-validator'
+import { EmailValidator } from '../protocols'
 
 interface SutTypes {
   sut: SignUpController
